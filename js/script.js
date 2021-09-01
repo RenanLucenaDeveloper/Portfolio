97ba19a0735717e0096a4a08bb3178fdcf560666
RenanLucenaDeveloper <reenanluceena@gmail.com>
2021-09-01 09:55:20 -0300

    slide

---
+++ js/script.js
@@ -0,0 +1,7 @@
+new SimpleSlide({
+  slide: 'principal',
+  nav: true, // se deve ou não mostrar a navegação
+  auto: true, // se o slide deve passar automaticamente
+  time: 8000, // tempo de transição dos slides
+  pauseOnHover: true, // pausa a transição automática
+});
