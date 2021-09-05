0f98f999619af4a5abc44f4ed0dcbb207adfe882
RenanLucenaDeveloper <reenanluceena@gmail.com>
2021-09-05 20:09:45 -0300

    mobile v1

--- js/script.js
+++ js/script.js
@@ -6,6 +6,6 @@ new SimpleSlide({
   slide: 'principal',
   nav: true, // se deve ou não mostrar a navegação
   auto: true, // se o slide deve passar automaticamente
-  time: 8000, // tempo de transição dos slides
+  time: 9000, // tempo de transição dos slides
   pauseOnHover: true, // pausa a transição automática
 });
